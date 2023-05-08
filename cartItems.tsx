1b64520e6e304b5110ee06e75a48fd39b54155e0
manlikexavy <chibokaxavier@gmail.com>
2023-05-08 10:01:56 -0700

    UI of the body completed

--- cartItems.tsx
+++ cartItems.tsx
@@ -3,7 +3,7 @@ const menu = [
     {
       id: 1,
       title: 'buttermilk pancakes ',
-      category: 'breakfast',
+      amount: 1,
       price: 5000,
       img: '/images/item-1.jpeg',
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
@@ -11,7 +11,7 @@ const menu = [
     {
       id: 2,
       title: 'diner double',
-      category: 'lunch',
+      amount:1,
       price: 7500,
       img: '/images/item-2.jpeg',
       desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
@@ -19,7 +19,7 @@ const menu = [
     {
       id: 3,
       title: 'godzilla milkshake',
-      category: 'shakes',
+      amount: 1,
       price: 12000,
       img: '/images/item-3.jpeg',
       desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
@@ -27,7 +27,7 @@ const menu = [
     {
       id: 4,
       title: 'country delight',
-      category: 'breakfast',
+      amount: 1,
       price: 4100,
       img: '/images/item-4.jpeg',
       desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
@@ -35,7 +35,7 @@ const menu = [
     {
       id: 5,
       title: 'egg attack',
-      category: 'lunch',
+      amount: 1,
       price: 6000,
       img: '/images/item-5.jpeg',
       desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
@@ -43,7 +43,7 @@ const menu = [
     {
       id: 6,
       title: 'oreo dream',
-      category: 'shakes',
+      amount: 1,
       price: 50000,
       img: '/images/item-6.jpeg',
       desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
@@ -51,7 +51,7 @@ const menu = [
     {
       id: 7,
       title: 'bacon overflow',
-      category: 'breakfast',
+      amount: 1,
       price: 20000,
       img: '/images/item-7.jpeg',
       desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
@@ -59,7 +59,7 @@ const menu = [
     {
       id: 8,
       title: 'american classic',
-      category: 'lunch',
+      amount:1,
       price: 100000,
       img: '/images/item-8.jpeg',
       desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
@@ -67,7 +67,7 @@ const menu = [
     {
       id: 9,
       title: 'quarantine buddy',
-      category: 'shakes',
+      amount: 1,
       price: 30000,
       img: '/images/item-9.jpeg',
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,20 +1,64 @@
-import React from 'react'
+import React from "react";
 
 export interface Cart {
-    id: number;
-    title: string;
-    category: string;
-    price: number;
-    img: string;
-    desc: string;
-  }
-  
-
-const CartItem = ({item}) => {
-    const {amount,id,title,category,price,img,desc} =item
-  return (
-    <div className='pt-4'>{title}</div>
-  )
+  id: number;
+  title: string;
+  category: string;
+  price: number;
+  img: string;
+  desc: string;
 }
 
-export default CartItem
+const CartItem = ({ item }) => {
+  const { amount, id, title, category, price, img, desc } = item;
+  return (
+    <div className=" pt-4 flex justify-between">
+      <div className=" flex">
+        <div className="h-[100px] w-[75px]">
+          <img src={img} alt={title} className="h-full w-full" />
+        </div>
+        <div className="ml-[80px]">
+          <span className="capitalize">{title}</span>
+          <p className="text-gray-500">${price}</p>
+          <button className="bg-red-600  p-2 rounded-md text-white uppercase ">
+            Remove
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center  ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+          />
+        </svg>
+        <span>{amount}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
